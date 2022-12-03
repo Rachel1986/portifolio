23cf30db0a661badcff4f96d62bbda50843de514
Rachel1986 <rachelguzmanv@yahoo.com>
2022-12-03 20:27:48 -0300

    Login e componente header - Implementado Lazy loading

---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyB3L2mVjgyitupVkgow60zlnXNssz6WIuM",
+    authDomain: "portifolio-2da50.firebaseapp.com",
+    projectId: "portifolio-2da50",
+    storageBucket: "portifolio-2da50.appspot.com",
+    messagingSenderId: "383801729836",
+    appId: "1:383801729836:web:448283ef078d54e4c54289"
+}
+};
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
